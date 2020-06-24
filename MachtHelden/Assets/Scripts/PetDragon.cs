521f11e7e57038a722528c6f8957d095ce9aadc3
Robin Daraban <robin.daraban@gmx.de>
2020-06-24 17:28:20 +0200

    Add Dragons to show Power Level

--- MachtHelden/Assets/Scripts/PetDragon.cs
+++ MachtHelden/Assets/Scripts/PetDragon.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PetDragon : MonoBehaviour
 {
+    [SerializeField]
+    float averageHeight;
+    [SerializeField]
+    Renderer render;
+    [SerializeField]
+    public float amplitude;
+    [SerializeField]
+    public float frequency;
+    [SerializeField]
+    public float degreePerSecond;
+    public Transform target;
+
+
+    float timeAlive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        averageHeight = transform.localPosition.y;
+        render.material.color  = Random.ColorHSV(0, 1, .5f, .7f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeAlive += Time.deltaTime;
+        transform.RotateAround(target.position, Vector3.up, degreePerSecond * Time.deltaTime);
+        float altitude = averageHeight + amplitude * Mathf.Sin(timeAlive * frequency *Mathf.PI*2);
+        transform.localPosition = new Vector3(transform.localPosition.x, altitude, transform.localPosition.z);
     }
 }
---
+++ MachtHelden/Assets/Scripts/PetDragonSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetDragonSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    PlayerController observedPlayer;
+    [SerializeField]
+    float respawnTime = 1;
+    [SerializeField]
+    PetDragon dragonPrefab;
+    [SerializeField]
+    float minRadius;
+    [SerializeField]
+    float maxRadius;
+    [SerializeField]
+    float amplitudeMultiplier;
+    [SerializeField]
+    float speedMultiplier;
+
+    float spawnTimer = 1;
+    int dragonsOut = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if(spawnTimer <0 && dragonsOut < MaximumDragons(observedPlayer.powerLevel)){
+            dragonsOut++;
+            spawnTimer = respawnTime + Random.Range(0, respawnTime);
+            Vector3 spawnPos = observedPlayer.transform.position + new Vector3(0, 1, -Random.Range(minRadius, maxRadius));
+            PetDragon newDragon = Instantiate(dragonPrefab, spawnPos, Quaternion.identity, observedPlayer.transform);
+            newDragon.amplitude *= Vector3.Distance(newDragon.transform.position, observedPlayer.transform.position) * amplitudeMultiplier;
+            newDragon.degreePerSecond *= Vector3.Distance(newDragon.transform.position, observedPlayer.transform.position) * speedMultiplier;
+            newDragon.target = observedPlayer.transform;
+        }
+    }
+
+    Vector3 SpawnPosition() {
+        return observedPlayer.transform.position + new Vector3(0,1,-Random.Range(minRadius, maxRadius));
+    }
+
+    int MaximumDragons(int powerLevel) {
+        return powerLevel;
+    }
+}
